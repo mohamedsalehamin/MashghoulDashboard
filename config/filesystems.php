03cead957577a0f448f14ec5bec288fd60acb59f
Your Name <you@example.com>
2024-12-18 13:19:31 +0100

    wip

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,8 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-//            'url' => request()->getSchemeAndHttpHost().'/storage',
+'url' => env('APP_URL').'/storage',
+            //'url' => request()->getSchemeAndHttpHost().'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
