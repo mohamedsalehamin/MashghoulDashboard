ecf1fbd673ac8be69f277b4689f7dc99bded87b5
Ahmed Mostafa <ahmed.mostafa.dev.eg@gmail.com>
2025-01-18 21:12:27 +0200

    wip

--- panels/default/src/Resources/Api/Customer/SeatResource.php
+++ panels/default/src/Resources/Api/Customer/SeatResource.php
@@ -7,12 +7,13 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class SeatResource extends JsonResource {
 
     public function toArray($request) {
+
         return [
             'id' => $this->id,
             'title' => $this->title,
             'services' => ServiceResource::collection($this->services()->enabled()->get()),
 
-            'working_days' =>WorkingTimesResource::collection(collect( $this->meta_data['days_list']??[])->map(fn($slot)=>collect($slot)->where('status',1))),
+            'working_days' => collect(WorkingTimeSlotsResource::collection(collect( $this->meta_data['days_list']??[])->map(fn($slot)=>collect($slot)->where('status',1))))->filter(fn($slot)=>$slot->isNotEmpty())->values(),
         ];
     }
 }
